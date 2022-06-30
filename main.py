8a67c344a0e7a4a6f5761b41c1c9854f54056074
GageCorp <corp.gage@gmail.com>
2022-06-29 19:00:41 -0700

    added all bids entered condition

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from art import logo
 import os
 
 bids = []
-all_bids_entered = False
 
 print(logo)
 print("Welcome to the secret auction.")
 
 
-while all_bids_entered == False:
-    name = input("What is your name?:")
+while True:
+    name = input("What is your name?: ")
     bid = int(input("What is your bid?: "))
     bids.append({
         "Name": name,
         "Bid" : bid
     })
-    more_bidders = input("Are there any other biders? Type \'yes\' or \'no\' ")
+    more_bidders = input("Are there any other biders? Type \'yes\' or \'no\': ")
+    os.system('cls')
+    if more_bidders == "no":
+        break
+    
