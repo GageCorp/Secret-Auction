b8b9647f100c00075a4f68d5f50f3426db246560
GageCorp <corp.gage@gmail.com>
2022-07-18 18:59:13 -0700

    issa done

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 from art import logo
 import os
 
-bids = []
+bids = {}
 
 print(logo)
 print("Welcome to the secret auction.")
 
 bidding_finished = False
 
+def highest_bidder(bids):
+    highest_bid = 0
+    winner = ''
+    for bid_placed in bids:
+        bid_amount = bids[bid_placed]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bid_placed
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
 while not bidding_finished:
     name = input("What is your name?: ")
     bid = int(input("What is your bid?: "))
-    bids.append({
-        "Name": name,
-        "Bid" : bid
-    })
+    bids[name] = bid
     more_bidders = input("Are there any other biders? Type \'yes\' or \'no\': ")
     os.system('cls')
     if more_bidders == "no":
         bidding_finished = True
 
+highest_bidder(bids)
 
-
-def highest_bidder(bids):
-    highest_bid = 0
-    winner = ''
-    for bid in bids:
-        bid_amount = bids[bid]
-        if bid_amount > highest_bid:
-            highest_bid = bid_amount
-            
