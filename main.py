99d9656051a65b4cbc30c1b3b84d5162d9ffab5e
GageCorp <corp.gage@gmail.com>
2022-07-18 18:48:06 -0700

    Back at it again

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ bids = []
 print(logo)
 print("Welcome to the secret auction.")
 
+bidding_finished = False
 
-while True:
+while not bidding_finished:
     name = input("What is your name?: ")
     bid = int(input("What is your bid?: "))
     bids.append({
@@ -17,5 +18,15 @@ while True:
     more_bidders = input("Are there any other biders? Type \'yes\' or \'no\': ")
     os.system('cls')
     if more_bidders == "no":
-        break
-    
+        bidding_finished = True
+
+
+
+def highest_bidder(bids):
+    highest_bid = 0
+    winner = ''
+    for bid in bids:
+        bid_amount = bids[bid]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            
